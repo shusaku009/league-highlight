87782dd708b623d04d210dfb4ca668537a9fe488
shusaku009 <shusaku009@gmail.com>
2023-07-07 06:47:21 +0900

    add チームとリーグのシードファイルを作成 #5

---
+++ db/seeds/01_league_category.rb
@@ -0,0 +1,6 @@
+# リーグ一覧
+LeagueCategory.create(name: 'プレミアリーグ')
+LeagueCategory.create(name: 'ラ・リーガ')
+LeagueCategory.create(name: 'ブンデスリーガ')
+LeagueCategory.create(name: 'セリエA')
+LeagueCategory.create(name: 'リーグ・アン')
