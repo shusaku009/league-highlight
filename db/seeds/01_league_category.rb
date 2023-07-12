bbe4446a36852f0c91d07e5c21ae7b1eea161089
shusaku009 <shusaku009@gmail.com>
2023-07-12 14:40:06 +0900

    remove LeagueCategoryのシードファイルを削除 #5

--- db/seeds/01_league_category.rb
+++
@@ -1,6 +0,0 @@
-# リーグ一覧
-LeagueCategory.create(name: 'プレミアリーグ')
-LeagueCategory.create(name: 'ラ・リーガ')
-LeagueCategory.create(name: 'ブンデスリーガ')
-LeagueCategory.create(name: 'セリエA')
-LeagueCategory.create(name: 'リーグ・アン')
