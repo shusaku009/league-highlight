a0ceda42ea7a11edf28010091a87695acc00a8d2
shusaku009 <shusaku009@gmail.com>
2023-07-15 17:07:24 +0900

    add teamコントローラーを作成 #5

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -2,19 +2,10 @@
 #
 # Table name: teams
 #
-#  id                 :bigint           not null, primary key
-#  name               :string(255)      not null
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
-#  league_category_id :bigint           not null
-#
-# Indexes
-#
-#  index_teams_on_league_category_id  (league_category_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (league_category_id => league_categories.id)
+#  id         :bigint           not null, primary key
+#  name       :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #
 require 'rails_helper'
 
