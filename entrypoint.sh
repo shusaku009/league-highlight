1702c571968fd5d052563adea1a01f0fdea900a9
shusaku009 <shusaku009@gmail.com>
2023-07-17 16:41:56 +0900

    add dockerでWheneverが使えるように設定 #7

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,11 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /league_highlight/tmp/pids/server.pid
 
+# cronデーモンの起動
+service cron start
+
+# cronタブの更新
+bundle exec whenever --update-crontab
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
