c643133fafc0cb749d5dd7a5f9edc3335c9af511
shusaku009 <shusaku009@gmail.com>
2023-07-30 20:02:11 +0900

    add 動画の詳細画面のいいねのテストを追加 #21

--- spec/system/likes_spec.rb
+++ spec/system/likes_spec.rb
@@ -6,10 +6,30 @@ RSpec.describe "Likes", type: :system do
     login_as(user)
   end
 
-  describe 'いいねといいねの解除' do
+  describe '動画の一覧画面のいいねといいねの解除' do
     let!(:video) {create(:video) }
     it 'いいねといいねの解除ができること' do
       visit '/videos'
+      expect {
+        within "#like_video_#{video.id}" do
+          find('.btn-like').click
+        end
+        sleep 0.1
+      }.to change { Like.count }.by(1)
+
+      expect {
+        within "#like_video_#{video.id}" do
+          find('.btn-unlike').click
+        end
+        sleep 0.1
+      }.to change { Like.count }.by(-1)
+    end
+  end
+
+  describe '動画の詳細画面のいいねといいねの解除' do
+    let!(:video) {create(:video) }
+    it 'いいねといいねの解除ができること' do
+      visit video_path(video)
       expect {
         within "#like_video_#{video.id}" do
           find('.btn-like').click
