533900966f0e18ef4b32c1022942b849d8e02afa
shusaku009 <shusaku009@gmail.com>
2023-07-31 19:48:43 +0900

    add 動画の詳細画面のお気に入り登録のテストを追加 #23

--- spec/system/bookmarks_spec.rb
+++ spec/system/bookmarks_spec.rb
@@ -29,4 +29,27 @@ RSpec.describe "Bookmarks", type: :system do
       }.to change { Bookmark.count }.by(-1)
     end
   end
+
+  describe '動画の詳細画面のお気に入りとお気に入りの解除' do
+    let!(:video) {create(:video) }
+    it 'お気に入りとお気に入りの解除ができること' do
+      visit video_path(video)
+      expect {
+        within "#bookmark_video_#{video.id}" do
+          find('.btn-bookmark').click
+          # 非同期処理が完了したことを確認する
+          expect(page).to have_selector('.btn-unbookmark')
+        end
+        # sleep 0.1
+      }.to change { Bookmark.count }.by(1)
+
+      expect {
+        within "#bookmark_video_#{video.id}" do
+          find('.btn-unbookmark').click
+          expect(page).to have_selector('.btn-bookmark')
+        end
+        # sleep 0.1
+      }.to change { Bookmark.count }.by(-1)
+    end
+  end
 end
