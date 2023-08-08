b6a3ec8ab077239562cb6a58dad3a71dd53070c0
shusaku009 <shusaku009@gmail.com>
2023-08-08 20:58:53 +0900

    add ゲストユーザーログイン機能を作成 #33

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/login', to: 'user_sessions#new'
   post '/login', to: 'user_sessions#create'
   delete '/logout', to: 'user_sessions#destroy'
+  post '/guest_login', to: 'user_sessions#guest_login'
 
   resources :password_resets, only: %i[new create edit update]
   resources :teams
