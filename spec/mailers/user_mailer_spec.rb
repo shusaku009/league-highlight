a8af48f4f73c31d7ca6442cf62b2751f64b0e0ee
shusaku009 <shusaku009@gmail.com>
2023-08-08 10:15:09 +0900

    add メーラーのテストを作成 #31

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,18 +1,32 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  describe "reset_password_email" do
-    let(:mail) { UserMailer.reset_password_email }
-
-    xit "renders the headers" do
-      expect(mail.subject).to eq("Reset password email")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+  describe "パスワードリセットのメールを送信" do
+    let(:user) { create(:user) }
+    let(:mail) { UserMailer.reset_password_email(user) }
+    before do
+      user.generate_reset_password_token!
+      mail.deliver_now
     end
 
-    xit "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    context 'メールを送信した時' do
+      it "renders the headers" do
+        expect(mail.subject).to eq('パスワードリセット')
+        expect(mail.to).to eq([user.email])
+        expect(mail.from).to eq(["from@example.com"])
+      end
+      
+      it "メール本文が正しい" do
+        expect(mail.html_part.body.to_s).to have_content("#{user.user_name}様")
+        expect(mail.html_part.body.to_s).to have_content('パスワード再発行のご依頼を受け付けました。')
+        expect(mail.html_part.body.to_s).to have_content('以下のリンクからパスワードの再発行を行ってください。')
+        expect(mail.html_part.body.to_s).to have_content("http://localhost:3001/password_resets/#{user.reset_password_token}/edit")
+        expect(mail.text_part.body.to_s).to have_content("#{user.user_name}様")
+        expect(mail.text_part.body.to_s).to have_content('パスワード再発行のご依頼を受け付けました。')
+        expect(mail.text_part.body.to_s).to have_content('以下のリンクからパスワードの再発行を行ってください。')
+        expect(mail.html_part.body.to_s).to have_content("http://localhost:3001/password_resets/#{user.reset_password_token}/edit")
+
+      end
     end
   end
-
 end
