4799241c38f0397c820d75d563600259974f2a1d
shusaku009 <shusaku009@gmail.com>
2023-08-12 22:07:20 +0900

    modify コメントを表示する際のN+1問題を解消 #43

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,13 +2,13 @@ class VideosController < ApplicationController
   def index
     @teams = Team.all
     @q = Video.ransack(params[:q])
-    @pagy, @videos = pagy(@q.result(distinct: true), items: 24)
+    @pagy, @videos = pagy(@q.result(distinct: true).order(published_at: :desc), items: 24)
   end
 
   def show
     @video = Video.find(params[:id])
     # 動画に対するコメントを表示
-    @pagy, @comments = pagy(@video.comments.order(created_at: :desc), items: 15)
+    @pagy, @comments = pagy(@video.comments.includes(:user).order(created_at: :desc), items: 15)
     @comment = Comment.new
   end
 end
