c2c751b28439924758630b72399e62d451f95589
shusaku009 <shusaku009@gmail.com>
2023-08-12 10:00:12 +0900

    add 管理画面にチームのCRUDを作成 #39

--- config/routes.rb
+++ config/routes.rb
@@ -36,5 +36,6 @@ Rails.application.routes.draw do
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
     resources :users, only: %i[index destroy]
+    resources :teams
   end
 end
