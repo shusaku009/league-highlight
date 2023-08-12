cca62df32e887234f0c8acb4ab74ae89b1faadb7
shusaku009 <shusaku009@gmail.com>
2023-08-12 13:54:17 +0900

    add 管理画面にコメント一覧表示と削除機能、ページネーションと検索機能を追加 #39

--- config/routes.rb
+++ config/routes.rb
@@ -38,5 +38,6 @@ Rails.application.routes.draw do
     resources :users, only: %i[index destroy]
     resources :teams
     resources :videos, only: %i[index destroy]
+    resources :comments, only: %i[index destroy]
   end
 end
