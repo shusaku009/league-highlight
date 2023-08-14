b451d2b94d2f1d505838a8169b5d5d62197b6565
shusaku009 <shusaku009@gmail.com>
2023-08-14 10:51:49 +0900

    add メタタグ・OGPを設定 #46

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,24 @@
 module ApplicationHelper
   include Pagy::Frontend
+  # rubocop:disable Metrics/MethodLength
+  def default_meta_tags
+    {
+      site: Settings.meta.site,
+      reverse: true,
+      title: Settings.meta.title,
+      description: Settings.meta.description,
+      keywords: Settings.meta.keywords,
+      canonical: request.original_url,
+      og: {
+        title: :full_title,
+        type: Settings.meta.og.type,
+        url: request.original_url,
+        image: image_url(Settings.meta.og.image_path),
+        site_name: :site,
+        description: :description,
+        locale: 'ja_JP'
+      }
+    }
+  end
+  # rubocop:enable Metrics/MethodLength
 end
