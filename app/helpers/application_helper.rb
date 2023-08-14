c7a202906e80073fcd72ac7fb40a857b0bd1dd23
shusaku009 <shusaku009@gmail.com>
2023-08-14 11:45:50 +0900

    modify metaタグのメソッドを修正 #46

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   include Pagy::Frontend
-  # rubocop:disable Metrics/MethodLength
   def default_meta_tags
     {
       site: Settings.meta.site,
@@ -9,16 +8,21 @@ module ApplicationHelper
       description: Settings.meta.description,
       keywords: Settings.meta.keywords,
       canonical: request.original_url,
-      og: {
-        title: :full_title,
-        type: Settings.meta.og.type,
-        url: request.original_url,
-        image: image_url(Settings.meta.og.image_path),
-        site_name: :site,
-        description: :description,
-        locale: 'ja_JP'
-      }
+      og: default_og
     }
   end
-  # rubocop:enable Metrics/MethodLength
+
+  private
+
+  def default_og
+    {
+      title: :full_title,
+      type: Settings.meta.og.type,
+      url: request.original_url,
+      image: image_url(Settings.meta.og.image_path),
+      site_name: :site,
+      description: :description,
+      locale: 'ja_JP'
+    }
+  end
 end
